e8d10e5260ae4954a66253c41d15a362da950941
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 21:25:24 +0000

    Adding tarfile member sanitization to extractall()

--- readzip.py
+++ readzip.py
@@ -10,12 +10,53 @@ except OSError as e:
 
 def extract_tgz(dirname, response):
     with tarfile.open(fileobj=io.BytesIO(response.content), mode="r:gz") as tarred:
-        tarred.extractall(dirname)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarred, dirname)
     print('file unzipped in '+ dirname)
     
 def extract_tar(dirname, response):
     with tarfile.open(fileobj=io.BytesIO(response.content), mode="r:") as tarred:
-        tarred.extractall(dirname)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarred, dirname)
     print('file unzipped in '+ dirname)
 
 def extract_zip(dirname, response):
